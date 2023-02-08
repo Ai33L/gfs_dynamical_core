ab6a5949240b6f01884cc15b6915b3fb10943985
Abel <abels2000@gmail.com>
2023-02-09 00:43:06 +0530

    remove climt code

--- gfs_dynamical_core/__init__.py
+++ gfs_dynamical_core/__init__.py
@@ -1,21 +1,11 @@
 # -*- coding: utf-8 -*-
 import sympl
 
-from ._core import (
-    get_default_state, get_grid, mass_to_volume_mixing_ratio,
-    get_interface_values, numpy_version_of,
-    bolton_q_sat, bolton_dqsat_dT, calculate_q_sat,
-    list_available_constants, set_constants_from_dict)
-
 from ._components import (GFSDynamicalCore)
 
 
 sympl.set_constant('top_of_model_pressure', 20., 'Pa')
 
-__all__ = (
-    get_default_state, get_grid, mass_to_volume_mixing_ratio, numpy_version_of,
-    get_interface_values, list_available_constants,
-    set_constants_from_dict,
-    bolton_q_sat, bolton_dqsat_dT, calculate_q_sat, GFSDynamicalCore)
+__all__ = (GFSDynamicalCore)
 
 __version__ = '0.16.14'
