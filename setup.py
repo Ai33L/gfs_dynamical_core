b761d60d9cecaaa24a6f76167faf3ac1f7a9d991
Abel <67835462+Ai33L@users.noreply.github.com>
2023-05-20 15:28:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -210,7 +210,8 @@ setup(
     packages=[
         'gfs_dynamical_core',
     ],
-    package_dir={'gfs_dynamical_core'},
+    package_dir={'gfs_dynamical_core':
+                'gfs_dynamical_core'},
     include_package_data=True,
     install_requires=requirements,
     cmdclass={
