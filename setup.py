7f62a76ae9b77d6ed2a4a5c347252171fcacf8ac
Abel <abels2000@gmail.com>
2023-05-28 15:14:54 +0530

    Bump version: 0.1.18 → 0.1.19

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ class gfs_bdist_wheel(native_bdist_wheel):
 
 setup(
     name='gfs_dynamical_core',
-    version='0.1.18',
+    version='0.1.19',
     description='Only the dynamical core of the climt repository',
     long_description=readme + '\n\n' + history,
     author="Rodrigo Caballero",
