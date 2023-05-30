0a1a05349bbded4a191c73a662d2c2a3ded35d57
Abel <abels2000@gmail.com>
2023-05-30 19:58:14 +0530

    Bump version: 0.1.29 → 0.1.30

--- setup.py
+++ setup.py
@@ -179,7 +179,7 @@ class gfs_bdist_wheel(native_bdist_wheel):
 
 setup(
     name='gfs_dynamical_core',
-    version='0.1.29',
+    version='0.1.30',
     description='Only the dynamical core of the climt repository',
     long_description=readme + '\n\n' + history,
     author="Abel Shibu",
