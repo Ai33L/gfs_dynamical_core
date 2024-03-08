cdede51360cb5c57dff8e0d4b56c9bccc8da1875
Abel Shibu <abels2000@gmail.com>
2024-03-08 16:01:00 +0530

    doc test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'gfs-dynamical-core'
+copyright = 'A'
+author = 'A'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.1.23'
+version = '0.1.23'
 
 # -- General configuration
 
