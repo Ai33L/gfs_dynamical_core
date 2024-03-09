95ef7db46a960ef556ccc91917e4e4f89a90c4e5
Abel Shibu <abels2000@gmail.com>
2024-03-09 11:27:34 +0530

    doc update

--- docs/conf.py
+++ docs/conf.py
@@ -119,6 +119,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
